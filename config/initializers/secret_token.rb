7fabfe53880a3e3a4077581fe76ae7a11fd9400b
P S Saravanan <saravanp@thoughtworks.com>
2013-08-07 11:32:45 +0530

    [Saravanan/DD] new skelton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Answerme::Application.config.secret_token = '8cba40dc5ac18bf2f4ab50d74454701ee617dbd0878814b7bd922e460f15822a177fbc87f0f1e2d7131b05b4f0e6590d9cac108c968508fb8cf5f5d0197b2142'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Answerme::Application.config.session_store :cookie_store, key: '_answerme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Answerme::Application.config.session_store :active_record_store
